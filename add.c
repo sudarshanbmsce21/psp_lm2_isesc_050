2c39ac21dec80841ea0a00997dc19266a61c801a
sudarshanbmsce21 <110719467+sudarshanbmsce21@users.noreply.github.com>
2022-08-06 16:35:50 +0530

    adding number is committed

---
+++ add.c
@@ -0,0 +1,11 @@
+//adding two numbers!
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("Enter two numbers to find its sum\n");
+  scanf("%d%d",&a,&b);
+  printf("sum is %d+%d=%d",a,b,a+b);
+    return 0;
+}
+  
